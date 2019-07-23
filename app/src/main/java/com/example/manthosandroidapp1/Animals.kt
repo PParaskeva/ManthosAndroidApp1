569757e66bd5ad9149c3bbc70892a24d51861d40
PParaskeva <pnagiotis@yahoo.gr>
2019-07-23 19:25:43 +0300

    Init the project

---
+++ app/src/main/java/com/example/manthosandroidapp1/Animals.kt
@@ -0,0 +1,3 @@
+package com.example.manthosandroidapp1
+
+data class Animals(val name:String,val age:Int)
---
+++ app/src/main/java/com/example/manthosandroidapp1/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.manthosandroidapp1
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button1.setOnClickListener {
+            println(button1.text)
+            colorSample.setBackgroundColor(Color.CYAN)
+        }
+        button2.setOnClickListener {
+            println(button2.text)
+            colorSample.setBackgroundColor(Color.GREEN)
+        }
+        button3.setOnClickListener {
+            println(button3.text)
+            colorSample.setBackgroundColor(Color.BLUE)
+        }
+        button4.setOnClickListener {
+            println(button4.text)
+            colorSample.setBackgroundColor(Color.RED)
+        }
+        button5.setOnClickListener {
+            println(button5.text)
+            colorSample.setBackgroundColor(Color.YELLOW)
+        }
+        button6.setOnClickListener {
+            println(button6.text)
+            colorSample.setBackgroundColor(Color.parseColor("#1aff66"))
+        }
+    }
+}
